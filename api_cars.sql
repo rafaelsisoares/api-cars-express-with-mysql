130238ac317a94b4d33d14183cf663bb11ff8a3b
Rafael Soares <r.soares2015@outlook.com>
2022-12-19 19:36:44 -0300

    fix: Correcoes no script SQL

--- api_cars.sql
+++ api_cars.sql
@@ -1,20 +1,20 @@
 USE apicarsdb;
 
 CREATE TABLE brands (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE fuels (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     type VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE models (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    brand_id TINYINT NOT NULL UNSIGN,
-    fuel_id TINYINT NOT NULL UNSIGN,
-    FOREIGN KEY (brand_id) REFERENCES brands(id),
-    FOREIGN KEY (fuel_id) REFERENCES fuels(id)
+    brand_id INT NOT NULL,
+    fuel_id INT NOT NULL,
+    CONSTRAINT fk_brand_id FOREIGN KEY (brand_id) REFERENCES brands(id),
+    CONSTRAINT fk_fuel_id FOREIGN KEY (fuel_id) REFERENCES fuels(id)
 );
