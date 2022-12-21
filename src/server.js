cc655c4495f552061ab3536d9abe3d7889f331e1
Rafael Soares <r.soares2015@outlook.com>
2022-12-20 23:20:55 -0300

    build: Configurando e testando a conexao com o DB

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
-const app = require('./app');
+require('dotenv').config();
 
-app.listen(3001, () => console.log('Server Online'));
+const app = require('./app');
+const connection = require('./db/connection');
+
+app.listen(3001, async () => {
+    console.log('Server Online');
+    const [result] = await connection.execute('SELECT 1');
+    if (result) console.log('Server connected to DB');
+});
