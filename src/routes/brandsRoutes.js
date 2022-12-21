5ac61b4fd26dc0d015bc498d35f3ea762bb4a7ce
Rafael Soares <r.soares2015@outlook.com>
2022-12-20 23:22:05 -0300

    fix: Corrigindo a mensagem de sucesso

--- src/routes/brandsRoutes.js
+++ src/routes/brandsRoutes.js
@@ -7,8 +7,8 @@ const brandsRouter = express.Router();
 brandsRouter.post('/', async (req, res) => {
     const { name } = req.body;
     try {
-        const [[result]] = await brandsDB.insert(name);
-        res.status(201).json({ message: `Marca ${name} registrada. ID: ${result.id}` });
+        const [result] = await brandsDB.insert(name);
+        res.status(201).json({ message: `Marca ${name} registrada. ID: ${result.insertId}` });
     } catch (err) {
         console.error(err.message);
         res.status(500).json({ message: 'Erro ao registrar a nova marca' });
