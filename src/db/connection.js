22d9eccfac26188ce658d67e6f14c31bc64cc7d6
Rafael Soares <r.soares2015@outlook.com>
2022-12-20 23:21:26 -0300

    fix: Corrigindo algumas variaveis de ambiente

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_ROOT_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTION,
     connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
