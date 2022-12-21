b1a1449bd4d7f1d64fe57b2bc7d1bf7fb7f748f3
Rafael Soares <r.soares2015@outlook.com>
2022-12-21 00:07:21 -0300

    feat: Criacao do endpoint GET /brands

--- src/routes/brandsRoutes.js
+++ src/routes/brandsRoutes.js
@@ -15,4 +15,14 @@ brandsRouter.post('/', async (req, res) => {
     };
 });
 
+brandsRouter.get('/', async (_req, res) => {
+    try {
+        const [result] = await brandsDB.getAllBrands();
+        res.status(200).json(result);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Erro ao fazer a busca' });
+    }
+});
+
 module.exports = brandsRouter;
