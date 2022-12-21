27bdf9ba0fec0f3402d3a3b0ae0800170003be13
Rafael Soares <r.soares2015@outlook.com>
2022-12-20 23:20:22 -0300

    chore: Importacao das rotas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const brandsRouter = require('./routes/brandsRoutes');
+
 const app = express();
+
+app.use(express.json());
+app.use('/brands', brandsRouter)
+
 
 module.exports = app;
