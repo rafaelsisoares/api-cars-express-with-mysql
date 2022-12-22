3481547100192024db5d94ee92311544dfd29bd0
Rafael Soares <r.soares2015@outlook.com>
2022-12-22 19:17:54 -0300

    feat: Criacao do endpoint PUT /cars/:id

--- src/routes/carsRoutes.js
+++ src/routes/carsRoutes.js
@@ -35,4 +35,19 @@ carsRouter.get('/:id', async (req, res) => {
     }
 });
 
+carsRouter.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [result] = await carsDB.update(req.body, id);
+        if (result.affectedRows > 0) {
+            res.status(200).json({ id, ...req.body });
+        } else {
+            res.status(404).json({ message: 'Carro não encontrado' });
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Erro ao fazer a atualizacao' });
+    }
+});
+
 module.exports = carsRouter;
