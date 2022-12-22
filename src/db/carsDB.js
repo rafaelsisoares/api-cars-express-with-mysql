c7bd076f3f058cb4276871ab43644be82c0aca5d
Rafael Soares <r.soares2015@outlook.com>
2022-12-22 19:17:27 -0300

    feat: Criacao da funcao update

--- src/db/carsDB.js
+++ src/db/carsDB.js
@@ -9,8 +9,14 @@ const getAll = () => conn.execute('SELECT * FROM models');
 
 const getById = (id) => conn.execute('SELECT * FROM models WHERE id = ?', [id]);
 
+const update = (data, id) => conn.execute(
+    `UPDATE models SET name = ?, brand_id = ?, fuel_id = ? WHERE id = ?`,
+    [data.name, data.brandId, data.fuelId, id]
+);
+
 module.exports = {
     insert,
     getAll,
     getById,
+    update,
 };
