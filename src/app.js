ff5a9e802a6399f6659857427a537d96846c6aa6
Rafael Soares <r.soares2015@outlook.com>
2022-12-21 21:11:34 -0300

    chore: Importacao das rotas referente aos carros

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const brandsRouter = require('./routes/brandsRoutes');
+const carsRouter = require('./routes/carsRoutes');
 
 const app = express();
 
 app.use(express.json());
-app.use('/brands', brandsRouter)
-
+app.use('/brands', brandsRouter);
+app.use('/cars', carsRouter);
 
 module.exports = app;
