6e7c393ef0b3c0786a54485b9aac4a2287211686
Rafael Soares <r.soares2015@outlook.com>
2022-12-22 20:05:38 -0300

    feat: Criacao do endpoint GET /fuels

---
+++ src/routes/fuelsRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const fuelsDB = require('../db/fuelsDB');
+
+const fuelsRouter = Router();
+
+fuelsRouter.get('/', async (_req, res) => {
+    try {
+        const [result] = await fuelsDB.getAll();
+        res.status(200).json(result);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Erro ao fazer a busca' });
+    }
+});
