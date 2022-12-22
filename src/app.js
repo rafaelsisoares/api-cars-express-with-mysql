93f307bf73eb5860cd8b35d33d3558c5e9dada74
Rafael Soares <r.soares2015@outlook.com>
2022-12-22 20:06:10 -0300

    chore: Importacao das rotas do endpoint /fuels

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const brandsRouter = require('./routes/brandsRoutes');
 const carsRouter = require('./routes/carsRoutes');
+const fuelsRouter = require('./routes/fuelsRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/brands', brandsRouter);
 app.use('/cars', carsRouter);
+app.use('/fuels', fuelsRouter);
 
 module.exports = app;
