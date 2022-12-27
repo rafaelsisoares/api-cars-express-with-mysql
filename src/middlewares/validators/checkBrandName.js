0346a42782888f9eb35dab35f5d2987e3104021e
Rafael Soares <r.soares2015@outlook.com>
2022-12-27 18:44:42 -0300

    feat: Adicao de mais uma validacao

--- src/middlewares/validators/checkBrandName.js
+++ src/middlewares/validators/checkBrandName.js
@@ -1,11 +1,16 @@
-module.exports = (req, res, next) => {
+const brandsDB = require('../../db/brandsDB');
+
+module.exports = async (req, res, next) => {
     const { name } = req.body;
+    const brands = await brandsDB.getAllBrands();
     if (!name) {
         return res.status(400).json({ message: 'O campo nome é obrigatório' });
     }
     if (name.length < 3 || typeof name !== 'string') {
         return res.status(400).json({ message: 'O nome inserido é inválido' });
     }
-
+    if (brands[0].some((b) => b.name.toLowerCase() === name.toLowerCase())) {
+        return res.status(400).json({ message: 'Essa marca já está registrada' });
+    }
     next();
 };
